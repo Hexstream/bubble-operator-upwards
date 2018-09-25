d4454cb3e3e76db527deb6ab09336d75d7b50c2e
Jean-Philippe Paradis <hexstream@hexstreamsoft.com>
2018-09-25 09:59:28 -0400

    Tests: Use proper argument order for IS.

--- tests/tests.lisp
+++ tests/tests.lisp
@@ -5,27 +5,23 @@
 (cl:in-package #:bubble-operator-upwards_tests)
 
 (define-test "featured-examples"
-  (is equal
-      (bubble-operator-upwards 'and '(plate (and ham eggs) beans))
-      '(and (plate ham beans) (plate eggs beans)))
-  (is equal
+  (is equal '(and (plate ham beans) (plate eggs beans))
+      (bubble-operator-upwards 'and '(plate (and ham eggs) beans)))
+  (is equal '((section 1 - "a") (section 1 - "b")
+              (section 2 - "a") (section 2 - "b")
+              (section 3 - "a") (section 3 - "b"))
       (bubble-operator-upwards 'alt '(section (alt 1 2 3) - (alt "a" "b"))
-                               :result-kind :branches)
-      '((section 1 - "a") (section 1 - "b")
-        (section 2 - "a") (section 2 - "b")
-        (section 3 - "a") (section 3 - "b")))
-  (is equal
+                               :result-kind :branches))
+  (is equal '(or
+              (>> ul (:class "important"))
+              (>> ul (:class "urgent"))
+              (>> ul (id "first-priority"))
+              (>> ol (:class "important"))
+              (>> ol (:class "urgent"))
+              (>> ol (id "first-priority")))
       (bubble-operator-upwards 'or '(>> (or ul ol)
                                      (or (:class (or "important" "urgent"))
-                                      (id "first-priority"))))
-      '(or
-        (>> ul (:class "important"))
-        (>> ul (:class "urgent"))
-        (>> ul (id "first-priority"))
-        (>> ol (:class "important"))
-        (>> ol (:class "urgent"))
-        (>> ol (id "first-priority"))))
-  (is equal
-      (cartesian-product '(1 2) '(a b c))
-      '((1 a) (1 b) (1 c)
-        (2 a) (2 b) (2 c))))
+                                      (id "first-priority")))))
+  (is equal '((1 a) (1 b) (1 c)
+              (2 a) (2 b) (2 c))
+      (cartesian-product '(1 2) '(a b c))))
